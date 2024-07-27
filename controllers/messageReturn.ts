3bfca220aa6d945ca86bbd6970edfde4ad6fe39b
Carlos Eduardo <cadu.giul@gmail.com>
2024-07-27 18:33:01 -0300

    novas funcionalidades

---
+++ controllers/messageReturn.ts
@@ -0,0 +1,8 @@
+export const error_message = ((message:string):object => {
+    return {'errorMessage': message}
+})
+
+export const success_message = ((message:string):object=>{
+    return {'successMessage': message}
+})
+
--- models/UserRepoitoryFuleiro.ts
+++ models/UserRepoitoryFuleiro.ts
@@ -1,11 +1,13 @@
 import { UserRepositoryInterface } from "./UserRepositoryInterface";
 import { User } from "./User";
+import {error_message} from "../controllers/messageReturn";
 
 export class UserRepositoryFuleiro implements UserRepositoryInterface{
     private users: User[] = [];
-    register(username: string, password: string, email: string, nome: string): void {
+    register(username: string, password: string, email: string, nome: string): object {
         const user: User = new User('p'+this.users.length,username,password,email,nome);
         this.users.push(user);
+        return this.users
     }
     login(username: string, password: string): object | null {
         const user = this.users.find(u=> u.username === username);
@@ -14,4 +16,9 @@ export class UserRepositoryFuleiro implements UserRepositoryInterface{
         }
         return null
     }
+    changePassoword(user:User, newPassword:string):object{
+        user.password = newPassword
+        return user
+    }
+
 }
